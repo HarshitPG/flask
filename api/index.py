6aaee211daf6c837aea53182c7e0c1c104c0db8f
Harshit P G <129543831+HarshitPG@users.noreply.github.com>
2024-01-14 11:57:07 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,35 @@
-from flask import Flask
+
+from flask import Flask, jsonify, render_template
+from flask import request
+import chess
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci(r"../server/engine/stockfish-windows-x86-64-modern.exe")
 
 app = Flask(__name__)
+ 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from the Flask API!(Flask Backend is functioning!)"}
+    return jsonify(data)
+
+# @app.route('/')
+# def root():
+#     return render_template("index.html")
+
+@app.route('/make_move',methods=["POST"])
+def make_move():
+    print("Received a POST request to /make_move")
+    print('request from',request.form)
+    fen = request.form.get('fen')
+    print('fen:',fen)
+    board = chess.Board(fen)
+    print(board)
+    result = engine.play(board,chess.engine.Limit(time=0.1))
+    board.push(result.move)
+    print('best_move:', str(result.move))
+    fen=board.fen()
+    return {'fen': fen,'best_move': str(result.move)}
